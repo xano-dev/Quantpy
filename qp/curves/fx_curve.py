55bd74b90b0cad2ef85f210f7a008380ba0aacf9
xano <62649990+Zen-ai@users.noreply.github.com>
2026-05-14 18:25:17 +1200

    fx_curve object and vectorised daycounts for multiple end dates and allow 2 currencies for holidays in daycount

---
+++ qp/curves/fx_curve.py
@@ -0,0 +1,120 @@
+from qp.utils.math.interpolation import InterpolationMethod, Interpolator
+from qp.time.daycount import Daycount, yearfrac
+from qp.utils.maps.currencies import Currency
+import datetime as dt
+import numpy as np
+
+
+class FXCurve:
+    """Python representation of an FX rate curve for a currency pair.
+
+    Tenors can be provided as dates or year fractions. If dates are provided,
+    they are converted to year fractions using the specified daycount convention.
+    The first tenor must always be 0, corresponding to the spot FX rate.
+
+    Args:
+        at_date: curve valuation date
+        daycount: daycount convention used for year fraction calculations
+        currency_1: first currency of the pair
+        currency_2: second currency of the pair
+        fx_rates: FX rates corresponding to each tenor, quoted as currency_2/currency_1
+        tenors: tenor dates or year fractions - first entry must be spot (0)
+        interpolation_method: interpolation method to use - default is log-linear
+        extrapolate: whether to allow extrapolation beyond the curve's tenor range
+
+    Example:
+        >>> curve = FXCurve(
+        ...     at_date=dt.date(2026, 5, 14),
+        ...     daycount=Daycount.ACT_360,
+        ...     currency_1=Currency.EUR,
+        ...     currency_2=Currency.USD,
+        ...     fx_rates=[1.16, 1.171, 1.173, 1.177],
+        ...     tenors=[dt.date(2026, 5, 14), dt.date(2026, 8, 14), dt.date(2027, 5, 14), dt.date(2028, 5, 14)],
+        ... )
+    """
+
+    def __init__(
+        self,
+        at_date: dt.date,
+        daycount: Daycount,
+        currency_1: Currency,
+        currency_2: Currency,
+        fx_rates: list[float],
+        tenors: list[dt.date] | list[float],
+        interpolation_method: InterpolationMethod = InterpolationMethod.LOG_LINEAR,
+        extrapolate: bool = False,
+    ):
+        self._at_date = at_date
+        self._daycount = daycount
+        self._currency_1 = currency_1
+        self._currency_2 = currency_2
+        self._fx_rates = fx_rates
+
+        if all(isinstance(item, dt.date) for item in tenors):
+            self._tenors = self._get_tenors(tenors)
+        else:
+            self._tenors = tenors
+
+        self._catch_errors()
+
+        self._interpolation_method = interpolation_method
+        self._extrapolate = extrapolate
+        self._interpolator = self._generate_interpolator()
+
+    @property
+    def at_date(self):
+        return self._at_date
+
+    @property
+    def daycount(self):
+        return self._daycount
+
+    @property
+    def currency_1(self):
+        return self._currency_1
+
+    @property
+    def currency_2(self):
+        return self._currency_2
+
+    @property
+    def fx_rates(self):
+        return self._fx_rates
+
+    @property
+    def tenors(self):
+        return self._tenors
+
+    @property
+    def interpolation_method(self):
+        return self._interpolation_method
+
+    @property
+    def extrapolate(self):
+        return self._extrapolate
+
+    @property
+    def spot_rate(self):
+        return self._fx_rates[0]
+
+    def _catch_errors(self):
+        if self._tenors[0] != 0:
+            raise ValueError(
+                f"First tenor is {self._tenors[0]} - must be a spot FX rate (i.e. tenor = 0)"
+            )
+
+    def _get_tenors(self, tenors: list[dt.date]):
+        return yearfrac(
+            self.at_date, tenors, self._daycount, self._currency_1, self._currency_2
+        )
+
+    def _generate_interpolator(self):
+        return Interpolator(
+            np.array(self._tenors),
+            np.array(self._fx_rates),
+            self._interpolation_method,
+            self._extrapolate,
+        )
+
+    def get_rate(self, tenor: float) -> float:
+        return self._interpolator.interpolate(tenor)
--- qp/time/daycount.py
+++ qp/time/daycount.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+import numpy as np
 from enum import StrEnum
 from qp.time.holiday_helper import get_holidays
 from qp.utils.maps.currencies import Currency
@@ -11,6 +12,9 @@ class Daycount(StrEnum):
     THIRTY_360 = "30/360"
     THIRTY_365 = "30/365"
     BUS_252 = "BUS/252"
+
+
+# --- Scalar helpers ---
 
 
 def get_thirty_days(start: dt.date, end: dt.date, denom: int) -> float:
@@ -25,9 +29,7 @@ def get_thirty_days(start: dt.date, end: dt.date, denom: int) -> float:
     if end_day == 31 and (start_day == 31 or start_day == 30):
         end_day = 30
 
-    thirty_days = total_year_days + total_month_days - start_day + end_day
-
-    return thirty_days
+    return total_year_days + total_month_days - start_day + end_day
 
 
 def get_actual_days(start: dt.date, end: dt.date) -> float:
@@ -56,16 +58,78 @@ def bus_252(start: dt.date, end: dt.date, hols: set[dt.date]) -> float:
     )
 
 
-def yearfrac(
-    start: dt.date, end: dt.date, daycount: Daycount, currency: Currency = None
-) -> float:
-    hols = (
-        None
-        if currency is None
-        else get_holidays(currency, years=tuple(range(start.year, end.year + 1)))
-    )
+# --- Vectorised helpers ---
 
-    yearfrac_fn = DAYCOUNT_FN_MAP.get(daycount)
+
+def get_thirty_days_vec(start: dt.date, ends: np.ndarray, denom: int) -> float:
+
+    years = ends.astype("datetime64[Y]").astype(int) + 1970
+    months = ends.astype("datetime64[M]").astype(int) % 12 + 1
+    days = (ends - ends.astype("datetime64[M]")).astype(int) + 1
+
+    total_year_days = (years - start.year) * denom
+    total_month_days = (months - start.month) * 30
+    start_day = 30 if start.day == 31 else start.day
+    end_day = np.where((days == 31) & ((start_day == 30) | (start_day == 31)), 30, days)
+
+    thirty_days = total_year_days + total_month_days - start_day + end_day
+
+    return thirty_days
+
+
+def get_actual_days_vec(start: dt.date, ends: np.ndarray) -> np.ndarray:
+    return (ends - np.datetime64(start, "D")).astype(int)
+
+
+def act_360_vec(start: dt.date, ends: np.ndarray) -> np.ndarray:
+    return get_actual_days_vec(start, ends) / 360
+
+
+def act_365_vec(start: dt.date, ends: np.ndarray) -> np.ndarray:
+    return get_actual_days_vec(start, ends) / 365
+
+
+def thirty_360_vec(start: dt.date, ends: np.ndarray) -> np.ndarray:
+    return get_thirty_days_vec(start, ends, 360) / 360
+
+
+def thirty_365_vec(start: dt.date, ends: np.ndarray) -> np.ndarray:
+    return get_thirty_days_vec(start, ends, 365) / 365
+
+
+def bus_252_vec(start: dt.date, ends: np.ndarray, hols: set[dt.date]) -> np.ndarray:
+    hols = np.array(sorted(hols), dtype="datetime64[D]")
+    return np.busday_count(start, ends, holidays=hols) / 252
+
+
+def yearfrac(
+    start: dt.date,
+    end: dt.date | list[dt.date],
+    daycount: Daycount,
+    currency_1: Currency = None,
+    currency_2: Currency = None,
+) -> float:
+    is_scalar = isinstance(end, dt.date)
+    max_year = end.year if is_scalar else max(end).year
+    ends = end if is_scalar else np.array(end, dtype="datetime64[D]")
+
+    def _get_currency_hols(currency):
+        return (
+            None
+            if currency is None
+            else get_holidays(currency, years=tuple(range(start.year, max_year + 1)))
+        )
+
+    currency_1_hols = _get_currency_hols(currency_1)
+    currency_2_hols = _get_currency_hols(currency_2)
+
+    hols = (currency_1_hols or set()).union(currency_2_hols or set())
+
+    yearfrac_fn = (
+        SCALAR_DAYCOUNT_FN_MAP.get(daycount)
+        if is_scalar
+        else VEC_DAYCOUNT_FN_MAP.get(daycount)
+    )
 
     if yearfrac_fn is None:
         raise KeyError(
@@ -74,15 +138,23 @@ def yearfrac(
 
     # business days needs holidays
     if daycount == Daycount.BUS_252:
-        return yearfrac_fn(start, end, hols)
+        return yearfrac_fn(start, ends, hols)
 
-    return yearfrac_fn(start, end)
+    return yearfrac_fn(start, ends)
 
 
-DAYCOUNT_FN_MAP = {
+SCALAR_DAYCOUNT_FN_MAP = {
     Daycount.ACT_360: act_360,
     Daycount.ACT_365: act_365,
     Daycount.THIRTY_360: thirty_360,
     Daycount.THIRTY_365: thirty_365,
     Daycount.BUS_252: bus_252,
+}
+
+VEC_DAYCOUNT_FN_MAP = {
+    Daycount.ACT_360: act_360_vec,
+    Daycount.ACT_365: act_365_vec,
+    Daycount.THIRTY_360: thirty_360_vec,
+    Daycount.THIRTY_365: thirty_365_vec,
+    Daycount.BUS_252: bus_252_vec,
 }
